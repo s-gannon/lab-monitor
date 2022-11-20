19b309bf1ee77c346144f11564cc6656a0c84516
Spencer Gannon <spencer.gannon@valpo.edu>
2022-11-20 12:37:34 -0600

    Moves test data to directory

---
+++ console_colors.py
@@ -0,0 +1,23 @@
+
+CEND      = '\33[0m' ; CGREY    = '\33[90m'
+CBOLD     = '\33[1m' ; CRED    = '\33[31m'
+CITALIC   = '\33[3m' ; CGREEN  = '\33[32m'
+CURL      = '\33[4m' ; CYELLOW = '\33[33m'
+CBLINK    = '\33[5m' ; CBLUE   = '\33[34m'
+CBLINK2   = '\33[6m' ; CVIOLET = '\33[35m'
+CSELECTED = '\33[7m' ; CBEIGE  = '\33[36m'
+CWHITE    = '\33[37m'; CBLACK  = '\33[30m'
+CBLACKBG  = '\33[40m'; CREDBG    = '\33[41m'
+CGREENBG  = '\33[42m'; CYELLOWBG = '\33[43m'
+CBLUEBG   = '\33[44m'; CVIOLETBG = '\33[45m'
+CBEIGEBG  = '\33[46m'; CWHITEBG  = '\33[47m'
+CRED2     = '\33[91m'; CGREEN2  = '\33[92m'
+CYELLOW2  = '\33[93m'; CBLUE2   = '\33[94m'
+CVIOLET2  = '\33[95m'; CBEIGE2  = '\33[96m'
+
+CWHITE2   = '\33[97m'
+
+CGREYBG    = '\33[100m'; CREDBG2    = '\33[101m'
+CGREENBG2  = '\33[102m'; CYELLOWBG2 = '\33[103m'
+CBLUEBG2   = '\33[104m'; CVIOLETBG2 = '\33[105m'
+CBEIGEBG2  = '\33[106m'; CWHITEBG2  = '\33[107m'
