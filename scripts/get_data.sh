82838e99307d037c2b5b468563b8246b7520fae3
Spencer Gannon <spencer.gannon@valpo.edu>
2022-12-27 10:57:26 -0600

    Create files in get_data script to write into

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -2,8 +2,12 @@
 
 DIR_PATH='/home/lab-monitor/lab-monitor'
 
+touch $DIR_PATH/data/data1.csv
 sshfs lab-monitor@euler.valpo.edu:$DIR_PATH/data/data.csv $DIR_PATH/data/data1.csv
+touch $DIR_PATH/data/data2.csv
 sshfs lab-monitor@germain.valpo.edu:$DIR_PATH/data/data.csv $DIR_PATH/data/data2.csv
+touch $DIR_PATH/data/data3.csv
 sshfs lab-monitor@katherine2.valpo.edu:$DIR_PATH/data/data.csv $DIR_PATH/data/data3.csv
+touch $DIR_PATH/data/data4.csv
 sshfs lab-monitor@rockhopper.valpo.edu:$DIR_PATH/data/data.csv $DIR_PATH/data/data4.csv
 
