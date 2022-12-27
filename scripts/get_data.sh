8bc62946567f4767fc7a4acaa7d77c7af5ac11ee
Spencer Gannon <spencer.gannon@valpo.edu>
2022-12-27 11:04:04 -0600

    Switched from sshfs to scp in get_data

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -2,12 +2,8 @@
 
 DIR_PATH='/home/lab-monitor/lab-monitor'
 
-touch $DIR_PATH/data/data1.csv
-sshfs lab-monitor@euler.valpo.edu:$DIR_PATH/data/data.csv $DIR_PATH/data/data1.csv
-touch $DIR_PATH/data/data2.csv
-sshfs lab-monitor@germain.valpo.edu:$DIR_PATH/data/data.csv $DIR_PATH/data/data2.csv
-touch $DIR_PATH/data/data3.csv
-sshfs lab-monitor@katherine2.valpo.edu:$DIR_PATH/data/data.csv $DIR_PATH/data/data3.csv
-touch $DIR_PATH/data/data4.csv
-sshfs lab-monitor@rockhopper.valpo.edu:$DIR_PATH/data/data.csv $DIR_PATH/data/data4.csv
+scp lab-monitor@euler.valpo.edu:$DIR_PATH/data/data.csv $DIR_PATH/data/data1.csv
+scp lab-monitor@germain.valpo.edu:$DIR_PATH/data/data.csv $DIR_PATH/data/data2.csv
+scp lab-monitor@katherine2.valpo.edu:$DIR_PATH/data/data.csv $DIR_PATH/data/data3.csv
+scp lab-monitor@rockhopper.valpo.edu:$DIR_PATH/data/data.csv $DIR_PATH/data/data4.csv
 
