cad8fcb08e4cbf0ccce399b6eaf0d87366e62df4
Spencer Gannon <spencer.gannon@valpo.edu>
2022-12-27 10:44:32 -0600

    Added computers to get_data list

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-sshfs lab-monitor@euler.valpo.edu:/home/lab-monitor/data.csv /home/lab-monitor/data/data1.csv
-sshfs lab-monitor@katherine2.valpo.edu:/home/lab-monitor/data.csv /home/lab-monitor/data/data2.csv
+sshfs lab-monitor@euler.valpo.edu:/home/lab-monitor/data/data.csv /home/lab-monitor/data/data1.csv
+sshfs lab-monitor@germain.valpo.edu:/home/lab-monitor/data/data.csv /home/lab-monitor/data/data2.csv
+sshfs lab-monitor@katherine2.valpo.edu:/home/lab-monitor/data/data.csv /home/lab-monitor/data/data3.csv
+sshfs lab-monitor@rockhopper.valpo.edu:/home/lab-monitor/data/data.csv /home/lab-monitor/data/data4.csv
 
