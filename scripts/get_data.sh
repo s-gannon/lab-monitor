db911247412a5b015593726f57e53fc281b78e8b
Spencer Gannon <spencer.gannon@valpo.edu>
2022-12-27 10:50:36 -0600

    Updated DIR_PATH var in get_data script

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -2,8 +2,8 @@
 
 DIR_PATH='/home/lab-monitor/lab-monitor'
 
-sshfs lab-monitor@euler.valpo.edu:$DIR_PATH/data/data.csv /home/lab-monitor/data/data1.csv
-sshfs lab-monitor@germain.valpo.edu:$DIR_PATH/data/data.csv /home/lab-monitor/data/data2.csv
-sshfs lab-monitor@katherine2.valpo.edu:$DIR_PATH/data/data.csv /home/lab-monitor/data/data3.csv
-sshfs lab-monitor@rockhopper.valpo.edu:$DIR_PATH/data/data.csv /home/lab-monitor/data/data4.csv
+sshfs lab-monitor@euler.valpo.edu:$DIR_PATH/data/data.csv $DIR_PATH/data/data1.csv
+sshfs lab-monitor@germain.valpo.edu:$DIR_PATH/data/data.csv $DIR_PATH/data/data2.csv
+sshfs lab-monitor@katherine2.valpo.edu:$DIR_PATH/data/data.csv $DIR_PATH/data/data3.csv
+sshfs lab-monitor@rockhopper.valpo.edu:$DIR_PATH/data/data.csv $DIR_PATH/data/data4.csv
 
