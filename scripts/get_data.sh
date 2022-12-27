fb7b69103521b27b8c1e0e1b17593e1231cfb304
Spencer Gannon <spencer.gannon@valpo.edu>
2022-12-27 13:06:46 -0600

    Better shell; accepts cl args or edit the HOSTS var

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
 DIR_PATH='/home/lab-monitor/lab-monitor'
+HOSTS='katherine2 rockhopper'
 
-scp -q lab-monitor@euler.valpo.edu:$DIR_PATH/data/data.csv $DIR_PATH/data/data1.csv
-scp -q lab-monitor@germain.valpo.edu:$DIR_PATH/data/data.csv $DIR_PATH/data/data2.csv
-scp -q lab-monitor@katherine2.valpo.edu:$DIR_PATH/data/data.csv $DIR_PATH/data/data3.csv
-scp -q lab-monitor@rockhopper.valpo.edu:$DIR_PATH/data/data.csv $DIR_PATH/data/data4.csv
-
+if [ $HOSTS = '' ]
+then
+	for host in $@
+	do
+		scp -q lab-monitor@$host:$DIR_PATH/data/data.csv $DIR_PATH/data/data1.csv
+	done
+else
+	for host in $HOSTS
+	do
+		scp -q lab-monitor@$host:$DIR_PATH/data/data.csv $DIR_PATH/data/data1.csv
+	done
+fi
